b57db7f776b8481747aa9fa35035cf92c76df9da
Lagat100 <weldonkiprop1@gmail.com>
2023-06-26 04:55:14 -0700

    reset value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 
 /**
  * reset_to_98 - resets pointer value to 98
- * n - pointer value
- *
+ * Return - pointer value
+ * 'n' - pointer value to be reet to 98
  */
-void reset_to_98(int *n) // Reset the value of the integer pointed to by 'n' to 98
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
