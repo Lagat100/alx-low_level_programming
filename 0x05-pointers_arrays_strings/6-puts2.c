7d979003d4f7dd4e0a931111d7fbc6c0a7c71565
Lagat100 <weldonkiprop1@gmail.com>
2023-06-26 10:56:09 -0700

    print characters of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * puts2 - prints every other character of a string starting with the first character
  * @str: string holding characters
  */
 void puts2(char *str)
 {
-	int i;
-
-	for (i = 0; i < _strlen(str); i++)
+	int a = 0;
+	
+	while (str[a] != '\0')
 	{
-		if (i % 2 == 0)
+		if (a % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(str[a]);
 		}
+		a++;
 	}
 	_putchar('\n');
 }
+
