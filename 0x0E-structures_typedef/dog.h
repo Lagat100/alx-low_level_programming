973953927fe2b2f4c7cc91ffad3abb14e591b205
Lagat100 <weldonkiprop1@gmail.com>
2023-07-17 13:57:18 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,31 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * main - checks the code
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Return: Always 0.
+ * Description: Longer description
  */
-int main()
+struct dog
 {
-	struct dog myDog;
-	
-	myDog.name = "Buddy";
-	myDog.age = 3.5;
-	myDog.owner = "Lagat";
+	char *name;
+	float age;
+	char *owner;
+};
 
-	printf("Name: %s\n", myDog.name);
-	printf("Age: %.1f\n", myDog.age);
-	printf("Owner: %s\n", myDog.owner);
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-	return 0;
-}	
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
